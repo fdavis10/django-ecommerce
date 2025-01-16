cd21978905bbe1bbd0e0f7ad4aeff123e7c7c663
Vladimir Telniy <fdavisarz10@gmail.com>
2025-01-16 23:01:20 +0300

    fix problem with size

--- my_site/main/views.py
+++ my_site/main/views.py
@@ -8,25 +8,6 @@ def popular_list(request):
     products = Product.objects.filter(category=pizza_category_popular)[:4]
     return render(request,'main/index/index.html', {'products':products})
 
-# def product_list(request):
-#     categories = Category.objects.all()
-#     products = Product.objects.filter(available=True)
-#     return render(request,
-#                   'main/product/list.html', {'products':products, 'categories':categories})
-# def product_list(request, category_slug=None):
-#     category = None
-#     categories = Category.objects.all()
-#     products = Product.objects.filter(available=True)
-#     if category_slug:
-#         category = get_object_or_404(Category,
-#                                      slug=category_slug)
-#     return render(request,
-#                   'main/product/list.html',
-#                   {'category': category,
-#                    'categories': categories,
-#                    'products': products,
-#                    'slug_url': category_slug})
-    
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -40,4 +21,4 @@ def product_list(request, category_slug=None):
         'category': category,
         'categories': categories,
         'products': products,
-    })
+        })
