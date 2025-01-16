7e2d474ba19337afa9e6d1e612d7826f2f774c85
Vladimir Telniy <fdavisarz10@gmail.com>
2025-01-16 21:09:25 +0300

    add menu and front

--- my_site/main/urls.py
+++ my_site/main/urls.py
@@ -5,4 +5,7 @@ app_name='main'
 
 urlpatterns = [
     path('', views.popular_list, name='popular_list'),
+    path('menu/', views.product_list, name='product_list'),
+    path('menu/category/<slug:category_slug>/', views.product_list,
+         name='product_list_by_category'),
 ]
--- my_site/main/views.py
+++ my_site/main/views.py
@@ -4,6 +4,40 @@ from .models import Product, Category
 
 
 def popular_list(request):
-    products = Product.objects.filter(available=True)[:4]
+    pizza_category_popular = Category.objects.filter(name__iexact="пицца").first()  
+    products = Product.objects.filter(category=pizza_category_popular)[:4]
     return render(request,'main/index/index.html', {'products':products})
 
+# def product_list(request):
+#     categories = Category.objects.all()
+#     products = Product.objects.filter(available=True)
+#     return render(request,
+#                   'main/product/list.html', {'products':products, 'categories':categories})
+# def product_list(request, category_slug=None):
+#     category = None
+#     categories = Category.objects.all()
+#     products = Product.objects.filter(available=True)
+#     if category_slug:
+#         category = get_object_or_404(Category,
+#                                      slug=category_slug)
+#     return render(request,
+#                   'main/product/list.html',
+#                   {'category': category,
+#                    'categories': categories,
+#                    'products': products,
+#                    'slug_url': category_slug})
+    
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category, available=True)
+    else:
+        products = Product.objects.filter(available=True).order_by('category')
+
+    return render(request, 'main/product/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products,
+    })
